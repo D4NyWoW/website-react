eaf20974a896eab3b191c75a7a4df49ad8f965b0
Alex <amaritei.alex@yahoo.com>
2023-04-10 10:45:55 +0300

    React-App

---
+++ my-app/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css'  
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+    <header>
+      <a href="/">amazona</a>
+    </header>
+    <main>list products</main>
+    </div>
+  )
+}
+
+export default App;
+
