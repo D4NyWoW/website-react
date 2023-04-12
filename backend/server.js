a53a925201416905c824c42608acfeef2d125e72
Alex <amaritei.alex@yahoo.com>
2023-04-12 15:10:39 +0300

    update

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,18 @@
 import express from "express";
 import data from "./data.js";
+import cors from "cors";
 // npm install nodemon --save-dev for update the server when I have a change in the server code
 // express is a function that called to return an object which is that express app
 // res.send() -> send data to the frontend
 const app = express();
+app.use(cors());
 
-app.get('/api/products', (req, res) => {
-    res.send(data.product);
+app.get("/api/products", (req, res) => {
+  return res.status(200).json({ products: data.products });
 });
 
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-    console.log(`serve at http://localhost:${port}`);
-})
+  console.log(`serve at http://localhost:${port}`);
+});
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,26 +1,44 @@
-import data from "../data";
+import { useEffect, useState } from "react";
+// import data from "../data"; it's static data from
+// inseatd data.products.map we will use prooducts.products.map
+import axios from "axios";
 import { Link } from "react-router-dom";
-// <a /> -> <Link />
+axios.defaults.baseURL = "http://localhost:5000"; // express base url
+// fetchData -> ajax request
+
 function HomeScreen() {
-    return <div>
-        <h1>Featured Products</h1>
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios.get("/api/products");
+      setProducts(result.data.products);
+    };
+    fetchData();
+  }, []);
+  return (
+    <div>
+      <h1>Featured Products</h1>
       <div className="products">
-        {data.products.map(product => (
+        {products.map((product) => (
           <div className="product" key={product.slug}>
-            <Link to={`/product/${product.slug}`}> 
-              <img src={product.image} alt={product.name}/>
+            <Link to={`/product/${product.slug}`}>
+              <img src={product.image} alt={product.name} />
             </Link>
             <div className="product-info">
-            <Link to={`/product/${product.slug}`}>
-              <p>{product.name}</p>
-            </Link>
-            <p><strong>$</strong>{product.price}</p>
-            <button>Add to Cart</button>
+              <Link to={`/product/${product.slug}`}>
+                <p>{product.name}</p>
+              </Link>
+              <p>
+                <strong>$</strong>
+                {product.price}
+              </p>
+              <button>Add to Cart</button>
             </div>
           </div>
         ))}
       </div>
     </div>
+  );
 }
 
 export default HomeScreen;
