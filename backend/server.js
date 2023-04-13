420acc3305aa8492d9c46610915a670301592d38
Alex <amaritei.alex@yahoo.com>
2023-04-13 11:56:27 +0300

    Product Screen

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,16 @@ app.use(cors());
 app.get("/api/products", (req, res) => {
   return res.status(200).json({ products: data.products });
 });
+// backend api for returning product information based on the slug of the product
+app.get("/api/products/slug/:slug", (req, res) => {
+  const product = data.product.find((x) => x.slug === req.params.slug);
+  // if product exist return the product
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: "Product Not Found" });
+  }
+});
 
 const port = process.env.PORT || 5000;
 
