1141efec3e6415fe702bf9dd5e93662dcacde8b0
Alex <amaritei.alex@yahoo.com>
2023-04-13 22:24:26 +0300

    rating-price-description-stock

--- backend/data.js
+++ backend/data.js
@@ -1,54 +1,54 @@
 const data = {
-    products: [
-        {
-            name: 'Nike Slim shirt',
-            slug: 'nike-slim-shirt1',
-            category: 'Shirts',
-            image:'/images/1.png',
-            price: 120,
-            countInStock: 10,
-            brand:'Nike',
-            rating: 4.5,
-            numReviews: 10,
-            description: 'high quality shirt'
-        },
-        {
-            name: 'Nike Slim shirt',
-            slug: 'nike-slim-shirt2',
-            category: 'Shirts',
-            image:'/images/2.png',
-            price: 120,
-            countInStock: 10,
-            brand:'Nike',
-            rating: 4.5,
-            numReviews: 10,
-            description: 'high quality shirt'
-        },
-        {
-            name: 'Nike Slim shirt',
-            slug: 'nike-slim-shirt3',
-            category: 'Shirts',
-            image:'/images/3.png',
-            price: 120,
-            countInStock: 10,
-            brand:'Nike',
-            rating: 4.5,
-            numReviews: 10,
-            description: 'high quality shirt'
-        },
-        {
-            name: 'Nike Slim shirt',
-            slug: 'nike-slim-shirt4',
-            category: 'Shirts',
-            image:'/images/4.png',
-            price: 120,
-            countInStock: 10,
-            brand:'Nike',
-            rating: 4.5,
-            numReviews: 10,
-            description: 'high quality shirt'
-        }
-    ]
-}
+  products: [
+    {
+      name: "Nike Slim shirt",
+      slug: "nike-slim-shirt1",
+      category: "Shirts",
+      image: "/images/1.png",
+      price: 120,
+      countInStock: 10,
+      brand: "Nike",
+      rating: 4.5,
+      numReviews: 10,
+      description: "high quality shirt",
+    },
+    {
+      name: "Nike Slim shirt",
+      slug: "nike-slim-shirt2",
+      category: "Shirts",
+      image: "/images/2.png",
+      price: 120,
+      countInStock: 0,
+      brand: "Nike",
+      rating: 4.5,
+      numReviews: 10,
+      description: "high quality shirt",
+    },
+    {
+      name: "Nike Slim shirt",
+      slug: "nike-slim-shirt3",
+      category: "Shirts",
+      image: "/images/3.png",
+      price: 120,
+      countInStock: 10,
+      brand: "Nike",
+      rating: 4.5,
+      numReviews: 10,
+      description: "high quality shirt",
+    },
+    {
+      name: "Nike Slim shirt",
+      slug: "nike-slim-shirt4",
+      category: "Shirts",
+      image: "/images/4.png",
+      price: 120,
+      countInStock: 10,
+      brand: "Nike",
+      rating: 4.5,
+      numReviews: 10,
+      description: "high quality shirt",
+    },
+  ],
+};
 
 export default data;
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -3,6 +3,11 @@ import { useEffect, useReducer } from "react";
 import { useParams } from "react-router-dom";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Card from "react-bootstrap/Card";
+import ListGroup from "react-bootstrap/ListGroup";
+import Badge from "react-bootstrap/Badge";
+import Button from "react-bootstrap/Button";
+import Rating from "../components/Rating";
 const reducer = (state, action) => {
   switch (action.type) {
     case "FETCH_REQUEST":
@@ -50,8 +55,57 @@ function ProductScreen() {
             alt={product.name}
           ></img>
         </Col>
-        <Col md={3}></Col>
-        <Col md={3}></Col>
+        <Col md={3}>
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              <h1>{product.name}</h1>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Rating>
+                rating={product.rating}
+                numReviews={product.numReviews}
+              </Rating>
+            </ListGroup.Item>
+            <ListGroup.Item>Price : ${product.price}</ListGroup.Item>
+            <ListGroup.Item>
+              Description:
+              <p>{product.description}</p>
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col md={3}>
+          <Card>
+            <Card.Body>
+              <ListGroup variant="flush">
+                <ListGroup.Item>
+                  <Row>
+                    <Col>Price:</Col>
+                    <Col>${product.price}</Col>
+                  </Row>
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <Row>
+                    <Col>Status:</Col>
+                    <Col>
+                      {product.countInStock > 0 ? (
+                        <Badge bg="success">In Stock</Badge>
+                      ) : (
+                        <Badge bg="danger">Unvailable</Badge>
+                      )}
+                    </Col>
+                  </Row>
+                </ListGroup.Item>
+                {product.countInStock > 0 && (
+                  <ListGroup.Item>
+                    <div className="d-grid">
+                      <Button variant="primary">Add to Cart</Button>
+                    </div>
+                  </ListGroup.Item>
+                )}
+              </ListGroup>
+            </Card.Body>
+          </Card>
+        </Col>
       </Row>
     </div>
   );
